f1580775a4e2a1779ecf00667ae1237956fda2fa
gtbarbosa <gtbarbosa@live.com>
2024-08-25 11:43:17 -0300

    feat: github actions

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "fiap-tech-challenge-terraform-state"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = var.eks_role_arn
